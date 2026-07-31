17a8f9ad6dfec8b70130f58c7195ed93e604145b
Soumabha Dey <soumabhadey76@gmail.com>
2026-07-31 21:22:21 +0530

    reverse string without strrev

---
+++ reverse_string_without_strrev.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    char input_string[128];
+
+    printf("Enter a string: ");
+    fgets(input_string, sizeof(input_string), stdin);
+
+    int length = strlen(input_string);
+
+    for (int i = 0; i < length / 2; i++) {
+        char tmp = input_string[i];
+        input_string[i] = input_string[length - i - 1];
+        input_string[length - i - 1] = tmp;
+    }
+
+    printf("Reversed string: %s\n", input_string);
+
+    return 0;
+}
